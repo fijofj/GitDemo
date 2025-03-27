56f43108967bd4f574e70e573930aa01d8e81142
fijofj <fijokj.stackuponline@gmail.com>
2025-03-27 22:19:30 +0530

    checkpoint

---
+++ GitDEmo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+string name;
+cout<<"enter your name:";
+cin>>name;
+cout<<"hello"<<name;
+
+
+}
---
+++ input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int i,j;
+    for(i=0;i<5;i++){
+        cout<<"pundachi\n";
+    }
+
+    return 0;
+}
